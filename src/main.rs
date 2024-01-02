ce817c47c0b605fb820d6d4817b812fa3e9da921
Thomas Lent <tlent@pm.me>
2024-01-01 21:45:18 -0500

    Initial version

--- src/main.rs
+++ src/main.rs
@@ -1,82 +1,59 @@
-use regex::Regex;
+use flate2::read::GzDecoder;
+use reqwest::header::{HeaderMap, ACCEPT, ACCEPT_ENCODING, ACCEPT_LANGUAGE, USER_AGENT};
+use scraper::{Html, Selector};
 use serenity::all::ChannelId;
-use serenity::async_trait;
-use serenity::http::Http;
-use serenity::model::channel::Message;
-use serenity::model::prelude::Ready;
 use serenity::prelude::*;
 use std::env;
-use std::sync::{Arc, OnceLock};
-use time::macros::time;
+use std::io::prelude::*;
 
-struct Handler;
+const NATIONAL_DAYS_BASE_URL: &str = "https://www.nationaldaycalendar.com";
+const NATIONAL_DAYS_READ_URL: &str = "https://www.nationaldaycalendar.com/read";
 
 #[tokio::main]
 async fn main() {
     let token = env::var("DISCORD_TOKEN").expect("Missing DISCORD_TOKEN environment variable");
     let intents = GatewayIntents::GUILD_MESSAGES | GatewayIntents::MESSAGE_CONTENT;
-    let mut client = Client::builder(&token, intents)
-        .event_handler(Handler)
+    let client = serenity::Client::builder(&token, intents)
         .await
         .expect("Error creating client");
-    tokio::spawn(national_day_interval(client.http.clone()));
-    if let Err(err) = client.start().await {
-        eprintln!("Client error: {err:?}");
-    }
-}
-
-#[async_trait]
-impl EventHandler for Handler {
-    async fn message(&self, context: Context, message: Message) {
-        static REGEX: OnceLock<Regex> = OnceLock::new();
-        let regex = REGEX.get_or_init(|| {
-            Regex::new(r"\b(https?://(?:www\.)?)(?:twitter\.com|x\.com)(/\S*)").unwrap()
-        });
-        let new_host = "fixupx.com";
-        let mut reply = String::new();
-        for captures in regex.captures_iter(&message.content) {
-            let (_, [before, after]) = captures.extract();
-            reply.reserve(before.len() + new_host.len() + after.len() + 1);
-            reply.push_str(before);
-            reply.push_str(new_host);
-            reply.push_str(after);
-            reply.push('\n');
-        }
-        if reply.is_empty() {
-            return;
-        }
-        if let Err(err) = message.channel_id.say(&context.http, reply).await {
-            eprintln!("Error sending message: {err:?}");
-        }
-    }
-
-    async fn ready(&self, _: Context, ready: Ready) {
-        println!("{} is connected", ready.user.name);
-    }
-}
-
-async fn national_day_interval(http: Arc<Http>) {
-    let now = time::OffsetDateTime::now_local().unwrap();
-    let mut start = now;
-    if start.time() >= time!(7:00) {
-        start += time::Duration::days(1);
-    }
-    start = start.replace_time(time!(7:00));
-    let duration_to_start = start - now;
-    let duration_to_start = std::time::Duration::new(
-        duration_to_start.whole_seconds() as u64,
-        duration_to_start.subsec_nanoseconds() as u32,
+    let mut headers = HeaderMap::new();
+    headers.insert(
+        ACCEPT,
+        "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"
+            .parse()
+            .unwrap(),
     );
-    let start = tokio::time::Instant::now() + duration_to_start;
-    let mut interval =
-        tokio::time::interval_at(start, std::time::Duration::from_secs(24 * 60 * 60));
-    loop {
-        interval.tick().await;
-        if let Err(err) = ChannelId::new(1191225413758894130)
-            .say(&http, "national days")
-            .await
-        {
-            eprintln!("Error saying national days: {err:?}");
-        }
+    headers.insert(ACCEPT_ENCODING, "gzip, deflate, br".parse().unwrap());
+    headers.insert(USER_AGENT, "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.2.1 Safari/605.1.15".parse().unwrap());
+    headers.insert(ACCEPT_LANGUAGE, "en-US,en;q=0.9".parse().unwrap());
+    let gzip_bytes = reqwest::Client::new()
+        .get(NATIONAL_DAYS_READ_URL)
+        .headers(headers)
+        .send()
+        .await
+        .expect("Error fetching national days page")
+        .bytes()
+        .await
+        .expect("Error getting response text");
+    let mut decoder = GzDecoder::new(&gzip_bytes[..]);
+    let mut html = String::new();
+    decoder
+        .read_to_string(&mut html)
+        .expect("Error decoding gzip html");
+    let document = Html::parse_document(&html);
+    let selector = Selector::parse(".m-card--header a").expect("Error parsing selector");
+    let element = document
+        .select(&selector)
+        .next()
+        .expect("Selector failed to find element");
+    let read_more_url = element.attr("href").expect("Element missing href");
+    let national_day_text = element.text().collect::<Vec<_>>().join("");
+    let message =
+        format!("{national_day_text}\n[Read more]({NATIONAL_DAYS_BASE_URL}{read_more_url})");
+    if let Err(err) = ChannelId::new(1191225413758894130)
+        .say(&client.http, message)
+        .await
+    {
+        eprintln!("Error saying message: {err:?}");
     }
 }
