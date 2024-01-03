28fea54c2566c0aafc90b78e258af6eec41aeba8
Thomas Lent <tlent@pm.me>
2024-01-02 22:35:38 -0500

    Add birthdays

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use rusqlite::OptionalExtension;
 use serenity::all::ChannelId;
 use serenity::prelude::*;
 use std::env;
@@ -23,15 +24,15 @@ async fn main() {
     let mut statement = db_connection
         .prepare("SELECT name, url FROM NationalDay WHERE occurrence_2024 = ?1")
         .expect("Failed preparing statement");
-    let day_iter = statement
-        .query_map([ymd_date], |row| Ok((row.get(0)?, row.get(1)?)))
+    let national_day_iter = statement
+        .query_map([&ymd_date], |row| Ok((row.get(0)?, row.get(1)?)))
         .expect("Statement query map failed");
     let mut message = date
         .format(format_description!(
             "[month repr:long] [day padding:none], [year]"
         ))
         .expect("Failed to format display date");
-    for day in day_iter {
+    for day in national_day_iter {
         let (name, url): (String, String) = day.unwrap();
         message.reserve(name.len() + url.len() + NATIONAL_DAY_BASE_URL.len() + 7);
         message.push_str(" | [");
@@ -40,6 +41,19 @@ async fn main() {
         message.push_str(NATIONAL_DAY_BASE_URL);
         message.push_str(&url);
         message.push(')');
+    }
+    let birthday: Option<String> = db_connection
+        .query_row(
+            "SELECT description FROM Birthday WHERE date = ?1",
+            [&ymd_date],
+            |row| row.get(0),
+        )
+        .optional()
+        .expect("Birthday query failed");
+    if let Some(s) = birthday {
+        message.reserve(s.len() + 3);
+        message.push_str(" | ");
+        message.push_str(&s);
     }
     if let Err(err) = ChannelId::new(CHANNEL_ID)
         .say(&serenity_client.http, message)
