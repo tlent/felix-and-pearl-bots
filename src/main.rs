2d03eed422a0d0342c6243c95e9023c3bc28fb8d
Thomas Lent <tlent@pm.me>
2024-01-03 15:02:25 -0500

    Clean up

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
-use reqwest::header::AUTHORIZATION;
-use reqwest::{Client, Response, Result};
+use anyhow::Result;
 use rusqlite::OptionalExtension;
 use std::env;
 use time::macros::format_description;
-use time::OffsetDateTime;
+use time::{Date, OffsetDateTime};
 
 const NATIONAL_DAY_BASE_URL: &str = "https://www.nationaldaycalendar.com";
 const DISCORD_API_URL: &str = "https://discord.com/api/v10";
@@ -11,63 +10,80 @@ const DB_PATH: &str = "days.db";
 const CHANNEL_ID: u64 = 1108866354980855961;
 
 #[tokio::main]
-async fn main() {
-    let discord_token =
-        env::var("DISCORD_TOKEN").expect("Missing DISCORD_TOKEN environment variable");
-    let db_connection = rusqlite::Connection::open(DB_PATH).expect("Failed to open db");
-    let http_client = Client::new();
+async fn main() -> Result<()> {
+    let discord_token = env::var("DISCORD_TOKEN")?;
+    let db_connection = rusqlite::Connection::open(DB_PATH)?;
+    let http_client = reqwest::Client::new();
     let date = OffsetDateTime::now_utc().date();
-    let ymd_date = date
-        .format(format_description!("[year]-[month]-[day]"))
-        .expect("Failed to format ymd date");
-    let mut statement = db_connection
-        .prepare("SELECT name, url FROM NationalDay WHERE occurrence_2024 = ?1")
-        .expect("Failed preparing statement");
-    let national_day_iter = statement
-        .query_map([&ymd_date], |row| Ok((row.get(0)?, row.get(1)?)))
-        .expect("Statement query map failed");
-    let mut message = date
-        .format(format_description!(
-            "[month repr:long] [day padding:none], [year]"
-        ))
-        .expect("Failed to format display date");
-    for day in national_day_iter {
-        let (name, url): (String, String) = day.unwrap();
-        message.reserve(name.len() + url.len() + NATIONAL_DAY_BASE_URL.len() + 7);
-        message.push_str(" | [");
-        message.push_str(&name);
-        message.push_str("](");
-        message.push_str(NATIONAL_DAY_BASE_URL);
-        message.push_str(&url);
-        message.push(')');
+    let ymd_date = date.format(format_description!("[year]-[month]-[day]"))?;
+    let national_days = get_national_days(&db_connection, &ymd_date)?;
+    let birthday = get_birthday(&db_connection, &ymd_date)?;
+    let message = build_message(date, &national_days, birthday.as_deref())?;
+    send_discord_message(&http_client, &discord_token, &message).await?;
+    Ok(())
+}
+
+fn get_national_days(
+    db_connection: &rusqlite::Connection,
+    ymd_date: &str,
+) -> Result<Vec<(String, String)>> {
+    let mut statement =
+        db_connection.prepare("SELECT name, url FROM NationalDay WHERE occurrence_2024 = ?1")?;
+    let rows = statement.query_map([&ymd_date], |row| Ok((row.get(0)?, row.get(1)?)))?;
+    let mut days = vec![];
+    for day in rows {
+        days.push(day?);
     }
-    let birthday: Option<String> = db_connection
+    Ok(days)
+}
+
+fn get_birthday(db_connection: &rusqlite::Connection, ymd_date: &str) -> Result<Option<String>> {
+    Ok(db_connection
         .query_row(
             "SELECT description FROM Birthday WHERE date = ?1",
             [&ymd_date],
             |row| row.get(0),
         )
-        .optional()
-        .expect("Birthday query failed");
+        .optional()?)
+}
+
+fn build_message(
+    date: Date,
+    national_days: &[(String, String)],
+    birthday: Option<&str>,
+) -> Result<String> {
+    let mut message = date.format(format_description!(
+        "[month repr:long] [day padding:none], [year]"
+    ))?;
+    for (name, url) in national_days {
+        message.reserve(name.len() + url.len() + NATIONAL_DAY_BASE_URL.len() + 7);
+        message.push_str(" | [");
+        message.push_str(name);
+        message.push_str("](");
+        message.push_str(NATIONAL_DAY_BASE_URL);
+        message.push_str(url);
+        message.push(')');
+    }
     if let Some(s) = birthday {
         message.reserve(s.len() + 3);
         message.push_str(" | ");
-        message.push_str(&s);
+        message.push_str(s);
     }
-    if let Err(err) = send_discord_message(&http_client, &discord_token, &message).await {
-        eprintln!("Error saying message: {err:?}");
-    }
+    Ok(message)
 }
 
 async fn send_discord_message(
-    client: &Client,
+    client: &reqwest::Client,
     discord_token: &str,
     message: &str,
-) -> Result<Response> {
-    client
+) -> Result<reqwest::Response> {
+    Ok(client
         .post(format!("{DISCORD_API_URL}/channels/{CHANNEL_ID}/messages"))
-        .header(AUTHORIZATION, format!("Bot {discord_token}"))
+        .header(
+            reqwest::header::AUTHORIZATION,
+            format!("Bot {discord_token}"),
+        )
         .body(format!("{{ \"content\": \"{message}\" }}"))
         .send()
-        .await
+        .await?)
 }
